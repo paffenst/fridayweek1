3224baed1b5e483020477e44ea090e50d52f5151
paffen <pavel.stoyanov0@hm.edu>
2023-04-21 15:26:25 +0200

    add isValidPassword function

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -1,7 +1,65 @@
 package de.neuefische;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("A password should be at least 8 characters long and no more than 15. " +
+                "\nIt should consist of upper and lower case letters," +
+                "\nnumbers and special characters (@,#,§,%,!,?) "  +
+                "\nand not be in a trivial one like 123456 or password: \n" +
+                "\nEnter your password: ");
+        String password = scanner.next();
+        isValidatedPass(password);
+        scanner.close();
+    }
+    /**
+     * function to check if password is valid
+     *
+     * @param password
+     * @return true
+     */
+    public static boolean isValidatedPass(String password) {
+        boolean isValid = true;
+        String numbers = "(.*[0-9].*)";
+
+        if (password.length() < 8) {
+            System.out.println("Password must be more than 8 characters.");
+            isValid = false;
+        }
+        if (password.length() > 15) {
+            System.out.println("Password must be less than 15 characters.");
+            isValid = false;
+        }
+        if (!password.matches(numbers ))
+        {
+            System.out.println("Password must have at least one number");
+            isValid = false;
+        }
+        String upperChars = "(.*[A-Z].*)";
+        if (!password.matches(upperChars ))
+        {
+            System.out.println("Password must have at least one uppercase character");
+            isValid = false;
+        }
+        String lowerChars = "(.*[a-z].*)";
+        if (!password.matches(lowerChars ))
+        {
+            System.out.println("Password must have at least one lowercase character");
+            isValid = false;
+        }
+        String specialChars = "(.*[@,#,§,%,!,?].*$)";
+        if (!password.matches(specialChars ))
+        {
+            System.out.println("Password must have at least one special character among @ # $ %");
+            isValid = false;
+        }
+        if (password.equals("passwort") || password.equals("123456")) {
+            System.out.println("Password must not be so trivial ;) ");
+            isValid = false;
+        }
+        return isValid;
     }
 }
